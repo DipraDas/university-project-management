25151ecc9c61e6b003e7f5208e6b3396c5441f57
Dipra <dipradas5940@gmail.com>
2024-01-13 21:07:43 +0600

    12.5

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,13 +1,28 @@
 import winston from 'winston';
+import path from 'path';
 
 const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+            level: 'info'
+        })
+    ]
+});
+
+const errorlogger = winston.createLogger({
     level: 'error',
     format: winston.format.json(),
     transports: [
         new winston.transports.Console(),
-        new winston.transports.File({ filename: 'error.log', level: 'info' }),
-        new winston.transports.File({ filename: 'combined.log', level: 'error' }),
-    ],
+        new winston.transports.File({
+            filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+            level: 'error'
+        })
+    ]
 });
 
-export default logger;
+export { logger, errorlogger };
